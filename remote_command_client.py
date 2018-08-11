a7c604c5fdc9f5077e2bd75a371355fe8d2f3009
luki009 <lukipalk@gmail.com>
2018-08-11 10:38:57 +0200

    Update remote_command_client.py

--- remote_command_client.py
+++ remote_command_client.py
@@ -55,6 +55,7 @@ def responseDispatcher(data):
             runCmd(cmd)
 
 def sendSocketData(message):
+    result = b""
     string_message = json.dumps(message)
     byte_message = string_message.encode('utf-8')
 
@@ -70,29 +71,30 @@ def sendSocketData(message):
             if not packet:
                 break
         print(data)
+        print(data.decode())
         # responseDispatcher(data.strip())
     finally:
         ssl_sock.close()
 
 if __name__ == "__main__":
-    # sendSocketData(_DATA)
-    string_message = json.dumps(_DATA)
-    byte_message = string_message.encode('utf-8')
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock = ssl.wrap_socket(sock, ca_certs=server_certificate, cert_reqs=ssl.CERT_REQUIRED)
-    sock.connect((server_ip, int(server_port)))
-    sock.sendall(byte_message)
-    sock.shutdown(socket.SHUT_WR)
-    try:
-        result = b''
-        while True:
-            packet = sock.recv(1024)
-            result += packet.strip()
-            if not packet:
-                break
-
-        print(result.decode('utf-8'))
-
-    finally:
-        sock.close()
+    sendSocketData(_DATA)
+    # string_message = json.dumps(_DATA)
+    # byte_message = string_message.encode('utf-8')
+    #
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # sock = ssl.wrap_socket(sock, ca_certs=server_certificate, cert_reqs=ssl.CERT_REQUIRED)
+    # sock.connect((server_ip, int(server_port)))
+    # sock.sendall(byte_message)
+    # sock.shutdown(socket.SHUT_WR)
+    # try:
+    #
+    #     while True:
+    #         packet = sock.recv(1024)
+    #         result += packet
+    #         if not packet:
+    #             break
+    #
+    #     print(result.decode('utf-8'))
+    #
+    # finally:
+    #     sock.close()
